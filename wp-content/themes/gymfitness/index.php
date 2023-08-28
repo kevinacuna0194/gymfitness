16b09b2254476936b09f3a2c903409f07d34e8a5
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-08-28 00:51:04 -0300

    Agregando el resto del contenido HTML

--- wp-content/themes/gymfitness/index.php
+++ wp-content/themes/gymfitness/index.php
@@ -1,6 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
 
-while (have_posts()) : the_post();
-    the_title();
-    the_content();
-endwhile;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<header>
+
+</header>
+
+<body>
+
+    <main>
+        <?php
+        while (have_posts()) : the_post();
+            the_title();
+            the_content();
+        endwhile;
+        ?>
+    </main>
+
+</body>
+
+</html>
